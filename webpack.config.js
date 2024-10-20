00f5f18ca2307e0987385d037a88f4a3c916c547
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-10-20 11:06:58 +0300

    create header section

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const path = require('path'); // Необходимо добавить этот импорт
 
 module.exports = {
@@ -7,17 +8,45 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devServer: {
+        port: 9000,
+        hot: true,
+        liveReload: true, // Включает автообновление страницы при изменениях
+        watchFiles: ['src/**/*', 'dist/**/*'],
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './dist/index.html', // Шаблон HTML-файла
+            template: './src/index.html', // Шаблон HTML-файла
+            filename: 'index.html', // Имя выходного HTML-файла
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
         }),
     ],
     module: {
         rules: [
             {
                 test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  'css-loader',
+                  'sass-loader',
+                ],
+              },
+              {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                  filename: 'fonts/[name][ext][query]', // Указывает папку для шрифтов
+                },
+              },
+              {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                generator: {
+                  filename: 'assets/images/[name][ext][query]', // Указывает папку для изображений
+                },
+              },
         ],
     },
     mode: 'development',
