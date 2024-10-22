b5a7343047a6cf7761a3bda28a6c80ca122b1681
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-10-22 10:47:18 +0300

    create custom selects

--- src/index.js
+++ src/index.js
@@ -3,8 +3,27 @@ import "./main.scss"
 
 document.addEventListener('DOMContentLoaded', () => {
 
+    const customSelects = document.querySelectorAll('.custom-select');
+    customSelects.forEach(select => {
+        select.addEventListener('click', () => {
+            select.classList.toggle('open');
+            const options = select.querySelector('.select-options');
+            const optionItems = options.querySelectorAll('.select-options__item');
+            options.classList.toggle('hide');
 
-    const slides = document.querySelectorAll('.slider__content-item');
+            optionItems.forEach(option => {
+                option.addEventListener('click', (e) => {
+                    e.stopPropagation();
+                    const selectedValue = option.textContent;
+                    select.querySelector(".select").textContent = selectedValue;
+                    options.classList.add('hide');
+                    optionItems.forEach(option => option.classList.remove('selected'));
+                    option.classList.add('selected');
+                });
+            })
+        });
+    })
+
     const dots = document.querySelectorAll('.slider__dot-item');
     let currentIndex = 0;
 
