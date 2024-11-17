0769240ce76c1a72c187271eb5a5120c784154ed
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-11-17 20:51:38 +0200

    add technique section

--- src/index.js
+++ src/index.js
@@ -24,27 +24,31 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     })
 
-    const dots = document.querySelectorAll('.slider__dot-item');
-    let currentIndex = 0;
-
-    function showSlide(index) {
-        const sliderContent = document.querySelector('.slider__content');
-        sliderContent.style.transform = `translateX(-${index * 100}%)`;
-        dots.forEach(dot => dot.classList.remove('dot-item-active'));
-        dots[index].classList.add('dot-item-active');
-    }
-
-    dots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            currentIndex = index;
-            showSlide(currentIndex);
+    const sliders = document.querySelectorAll(".slider");
+    sliders.forEach(slider => {
+        const dots = slider.querySelectorAll('.slider__dot-item');
+        let currentIndex = 0;
+    
+        function showSlide(index) {
+            const sliderContent = slider.querySelector('.slider__content');
+            sliderContent.style.transform = `translateX(-${index * 100}%)`;
+            dots.forEach(dot => dot.classList.remove('dot-item-active'));
+            dots[index].classList.add('dot-item-active');
+        }
+    
+        dots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                currentIndex = index;
+                showSlide(currentIndex);
+            });
         });
-    });
+    
+        // Инициализация первого слайда
+        showSlide(currentIndex);
+        // const autoSlide = setInterval(() => {
+        //     currentIndex = (currentIndex + 1) % dots.length;
+        //     showSlide(currentIndex);
+        // }, 5000);
+    })
 
-    // Инициализация первого слайда
-    showSlide(currentIndex);
-    // const autoSlide = setInterval(() => {
-    //     currentIndex = (currentIndex + 1) % dots.length;
-    //     showSlide(currentIndex);
-    // }, 5000);
 });
